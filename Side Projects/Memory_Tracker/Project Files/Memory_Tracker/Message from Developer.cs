c692b77f5845c5f13e69521912d995f22fdd27fc
CommanderKnight5214 <CommanderKnight5214@users.noreply.github.com>
2023-05-24 14:25:04 -0500

    Major Updates

---
+++ Side Projects/Memory_Tracker/Project Files/Memory_Tracker/Message from Developer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Memory_Tracker
+{
+    public partial class Message_from_Developer : Form
+    {
+        public Message_from_Developer()
+        {
+            InitializeComponent();
+        }
+
+        //This button will close this form and go back to the main program.
+        private void OKButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
